bf53561ce71f42b06c7e86c5e10e657315b43a3f
technology <technology@ces-103-1.nas.local>
2013-11-01 10:23:45 -0400

    Added distance method

--- chapt6/Countdown.java
+++ chapt6/Countdown.java
@@ -5,8 +5,10 @@ class Countdown {
     double r = 6.0;
  //   countdown(99);
     double a = area(r);
-    System.out.println("The area of the circle is: " + a);
-    System.out.println("The absolute value of -6.0 is " + absoluteValue(-6.0));
+    double dist = distance(1.0, 2.0, 4.0, 6.0);
+    System.out.println("Distance equals " + dist);
+//    System.out.println("The area of the circle is: " + a);
+//    System.out.println("The absolute value of -6.0 is " + absoluteValue(-6.0));
   }
 
   public static void countdown(int x)
@@ -24,6 +26,13 @@ class Countdown {
      return Math.PI * radius * radius;
   }
 
+  public static double distance(double x1, double y1, double x2, double y2)
+  {
+     double dx = x2 - x1;
+     double dy = y2 - y1;
+     double dsquared = dx*dx + dy*dy;
+     return Math.sqrt(dsquared);
+  }
 
   public static double absoluteValue(double x) 
   {
