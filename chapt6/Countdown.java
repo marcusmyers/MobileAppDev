e5cdb0438beff6c45370300804bfa90c54fa022e
marcusmyers <marcusmyers@gmail.com>
2013-11-04 10:29:56 -0500

    Added overloaded method area

--- chapt6/Countdown.java
+++ chapt6/Countdown.java
@@ -3,12 +3,10 @@ class Countdown {
   public static void main(String[] args)
   {
     double r = 6.0;
- //   countdown(99);
     double a = area(r);
-    double dist = distance(1.0, 2.0, 4.0, 6.0);
-    System.out.println("Distance equals " + dist);
-//    System.out.println("The area of the circle is: " + a);
-//    System.out.println("The absolute value of -6.0 is " + absoluteValue(-6.0));
+    double x = area(1.0, 2.0, 4.0, 6.0);
+    System.out.println("The area of circle one is: " + a);
+    System.out.println("The area of the circle is: " + x);
   }
 
   public static void countdown(int x)
@@ -26,6 +24,12 @@ class Countdown {
      return Math.PI * radius * radius;
   }
 
+  public static double area(double x1, double y1, double x2, double y2)
+  {
+    return area(distance(x1, y1, x2, y2));
+  }
+
+
   public static double distance(double x1, double y1, double x2, double y2)
   {
      double dx = x2 - x1;
@@ -33,6 +37,8 @@ class Countdown {
      double dsquared = dx*dx + dy*dy;
      return Math.sqrt(dsquared);
   }
+
+  
 
   public static double absoluteValue(double x) 
   {
