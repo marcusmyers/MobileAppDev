34a1ae293f5e1f8b5ee066e948c26ff338843ef0
Mark Myers <marcusmyers@gmail.com>
2013-11-07 10:28:23 -0500

    Added all code for step 1-4 of exercise 2 of chapter 6

--- chapt6/exercises/Multadd.java
+++ chapt6/exercises/Multadd.java
@@ -1,4 +1,29 @@
 // Chapter 6 - Exercise 6.2
 class MultAdd {
 
+  public static void main(String[] args)
+  {
+    double x = multadd(1.0, 2.0, 3.0);
+    System.out.println("5 =="+ x);
+    System.out.println(multadd(Math.sin(Math.PI/4),(Math.cos(Math.PI/4)/2)));
+    System.out.println(multadd(Math.log(10), Math.log(20)));
+  }  
+
+  public static double multadd(double a, double b, double c)
+  {
+    return a*b + c;
+  }
+
+  public static double multadd(double a, double b)
+  {
+	return a + b;
+  }
+
+  public static void yikes(double d)
+  {
+//    Math.sin();
+//    Math.cos();
+//    Math.log();
+//    Math.PI;
+  }
 }
