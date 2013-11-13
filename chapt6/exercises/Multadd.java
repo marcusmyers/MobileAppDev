637ded16b4ee8f0a2ce025089cb85752c84199f0
Mark Myers <marcusmyers@gmail.com>
2013-11-13 09:41:25 -0500

    Fixed Multadd.java file

--- chapt6/exercises/Multadd.java
+++ chapt6/exercises/Multadd.java
@@ -4,9 +4,9 @@ class MultAdd {
   public static void main(String[] args)
   {
     double x = multadd(1.0, 2.0, 3.0);
-    System.out.println("5 =="+ x);
     System.out.println(multadd(Math.sin(Math.PI/4),(Math.cos(Math.PI/4)/2)));
     System.out.println(multadd(Math.log(10), Math.log(20)));
+    System.out.println(yikes(6.0));   
   }  
 
   public static double multadd(double a, double b, double c)
@@ -16,14 +16,11 @@ class MultAdd {
 
   public static double multadd(double a, double b)
   {
-	return a + b;
+    return a + b;
   }
 
-  public static void yikes(double d)
+  public static double yikes(double d)
   {
-//    Math.sin();
-//    Math.cos();
-//    Math.log();
-//    Math.PI;
+    return multadd(d*Math.exp(-d), Math.sqrt(1-Math.exp(-d)));
   }
 }
