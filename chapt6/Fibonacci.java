8ecbe6a78466879661360831f5146248d2aef61b
Mark Myers <marcusmyers@gmail.com>
2013-11-19 09:34:50 -0500

    Modified Fibonacci file

--- chapt6/Fibonacci.java
+++ chapt6/Fibonacci.java
@@ -1,6 +1,6 @@
 class Fibonacci {
   public static void main(String[] args){
-    System.out.println(fibonacci(8));
+    System.out.println(fibonacci(4));
   }
 
   public static int fibonacci(int n){
